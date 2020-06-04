1cf51d40673223117a7a95f83f1b07fc2f67cf77
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2020-06-04 09:28:42 +0200

    [CI] should be closer to having a working travis config

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,7 +5,9 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install solr-jetty
+sudo apt-get install solr-jetty \
+                      libsasl2-dev \
+                      libldap2-dev
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
@@ -36,10 +38,13 @@ cd -
 
 echo "Installing ckanext-georchestra and its requirements..."
 python setup.py develop
+pip install -r requirements.txt
 pip install -r dev-requirements.txt
 
 echo "Moving test.ini into a subdir..."
 mkdir subdir
 mv test.ini subdir
+# update the path to test-core.ini
+sed -i -e 's|/usr/lib/ckan/lib/default/src/ckan/test-core.ini|ckan/test-core.ini|' subdir/test.ini
 
 echo "travis-build.bash is done."
